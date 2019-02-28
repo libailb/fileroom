085a323f9ddd664414fb8b25c859a96eba06b1f0
libailb <libailb@outlook.com>
2019-02-28 22:28:04 +0800

    file upload and download

---
+++ src/main/java/libai/aliyun/fileroom/FileroomApplication.java
@@ -0,0 +1,13 @@
+package libai.aliyun.fileroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/libai/aliyun/fileroom/FileroomApplicationTests.java
@@ -0,0 +1,16 @@
+package libai.aliyun.fileroom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FileroomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
